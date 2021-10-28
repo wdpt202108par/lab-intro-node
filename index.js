b9721c98da01374fc6c830f070c095f1272731ac
aude.claquesin <aude.claquesin@gmail.com>
2021-10-28 21:35:40 +0200

    Errors on Max, Min and avg

--- index.js
+++ index.js
@@ -1,17 +1,51 @@
 class SortedList {
-  constructor() {}
+  constructor() {
+    this.items = [];
+    this.length = this.items.length;
+  }
 
-  add(item) {}
+  add(item) {
+    this.items = this.items.concat(item);
+    this.items.sort(function (a,b) {
+      return a-b;
+    });
+    this.length = this.items.length;
+  }
 
-  get(pos) {}
+  get(pos) {
+    if (this.items[pos] < this.items.length) {
+      return this.items[pos];
+    } else {
+      throw new Error('OutOfBounds');
+    };
+  };
 
-  max() {}
+  max() {
+    if (this.items = []) {
+      throw new Error('EmptySortedList');
+    } else {
+      return Math.max(this.items);
+    };
+  };
 
-  min() {}
+  min() {
+    if (this.items = []) {
+      throw new Error('EmptySortedList');
+    } else {
+      return Math.min(this.items);
+    };
+  }
 
-  sum() {}
+  sum() {
+    return this.items.reduce((a,b) => a + b, 0)
+  }
 
-  avg() {}
+  avg() {
+    if (this.items = []) {
+      throw new Error('EmptySortedList');
+    } else {
+      return this.items.reduce((a,b) => a + b, 0) / this.items.length;
+  };
 }
 
 module.exports = SortedList;
