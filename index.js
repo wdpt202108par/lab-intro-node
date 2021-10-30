1c955b981d35aecab0936043f175a8cdd23cd5c5
Lea Beaulieu <beaulieu.lea@free.fr>
2021-10-30 10:59:39 +0200

    done iteration 3

--- index.js
+++ index.js
@@ -1,26 +1,31 @@
 class SortedList {
-  constructor(items, length) {
-    this.items = [];
-    this.length = this.items.length;
-  }
+    constructor(items, length) {
+        this.items = [];
+        this.length = this.items.length;
+    }
 
-  add(item) {
-    this.items.push(item);
-    this.length = this.items.length;
-    this.items.sort(function (a,b) {
-      return a-b;
-    });
-  }
+    add(item) {
+        this.items.push(item);
+        this.length = this.items.length;
+        this.items.sort(function(a, b) {
+            return a - b;
+        });
+    }
 
-  get(pos) {}
+    get(pos) {
+        if (pos > this.length) {
+            throw new Error('Out of Bounds');
+        }
+        return this.items[pos];
+    }
 
-  max() {}
+    max() {}
 
-  min() {}
+    min() {}
 
-  sum() {}
+    sum() {}
 
-  avg() {}
+    avg() {}
 }
 
 module.exports = SortedList;
