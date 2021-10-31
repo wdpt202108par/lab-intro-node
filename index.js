e75d9607e1e8fae269316eb6c611dfa8f6a1bf0a
Emma HB <emmanuelle.bolognini@gmail.com>
2021-10-31 16:26:30 +0100

    Lab with bonus done

--- index.js
+++ index.js
@@ -1,31 +1,55 @@
 class SortedList {
-    constructor(items, length) {
-        this.items = [];
-        this.length = this.items.length;
+  constructor(items, length) {
+    this.items = [];
+    this.length = this.items.length;
+  }
+
+  add(item) {
+    this.items.push(item);
+    this.length = this.items.length;
+    this.items.sort(function(a, b) {
+      return a - b;
+    });
+  }
+
+  get(pos) {
+    if (pos > this.length) {
+      throw new Error('Out of Bounds');
     }
+    return this.items[pos];
+}
 
-    add(item) {
-        this.items.push(item);
-        this.length = this.items.length;
-        this.items.sort(function(a, b) {
-            return a - b;
-        });
+  max() {
+    if (this.length <= 0) {
+      throw new Error('EmptySortedList');
     }
+    return this.items[this.length - 1]
+  }
 
-    get(pos) {
-        if (pos > this.length) {
-            throw new Error('Out of Bounds');
-        }
-        return this.items[pos];
+  min() {
+    if (this.length <= 0) {
+      throw new Error('EmptySortedList');
     }
+    return this.items[0]
+  }
 
-    max() {}
+  sum() {
+    if (this.length <= 0) {
+      return 0;
+    } else {
+      var total = this.items.reduce(function (accumulator, currentValue) {
+        return accumulator + currentValue
+      })
+      return total;
+    }
+  }
 
-    min() {}
-
-    sum() {}
-
-    avg() {}
+  avg() {
+    if (this.length <= 0) {
+      throw new Error('EmptySortedList');
+    }
+    return this.sum() / this.length;
+  }
 }
 
 module.exports = SortedList;
